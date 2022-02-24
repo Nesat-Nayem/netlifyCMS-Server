158a4b760b3e6e9a371a6cff7ae85b8acb832765
Nesat-Nayem <nesatnayem042@gmail.com>
2022-02-24 18:13:53 +0600

    capitalize

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 const { connectDB } = require("./config/connect");
 dotenv.config();
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -20,11 +20,12 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 app.use("/events", events);
+
 app.get("/", (req, res) => {
   res.json("hello world");
 });
 
 
 app.listen(port, () => {
-  console.log(`lisition ar port ${port}`);
+  console.log(`Server running port on ${port}`);
 });
